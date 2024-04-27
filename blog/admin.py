2c5e97f0d2b68126c4a690b943e5bcb62e361b5c
juanaregert <juanaregert@gmail.com>
2024-04-27 15:31:54 -0300

    creacion model_user

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
-from blog.models import Post ,Category,Comment
+from .models import Post ,Category,Comment
+from .models_user.custom_user import CustomUser
 # Register your models here.
 
 
-admin.site.register(Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','author','description','created_at','image ')
+    list_display = ('title','author','description','created_at',"image")
     search_fields = ('title',' description')
+    exclude = ('updated_at','categories',' slug','comments_count ','views_count ','like_count')
     
+@admin.register(CustomUser)
+class  CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("user","fecha_nac","bio","github")
+    exclude =("fecha_nac","bio","github")
 
-admin.site.register(Category)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display =('name','descripcion','slug')
-    exclude =('slug')
+    list_display = ('name','description','slug')
+    exclude =('slug',)
     
-admin.site.register(Comment)
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
- list_display =('author','description','create_at') 
+ list_display =('author','content','created_at') 
  
 
  
